f295dcfa34bf18ed6efd01d719dab4b994c837ee
Christian Hiland <hilandchristian112@gmail.com>
2023-04-20 22:49:28 +0000

    modified:   Host.py 	new file:   templates/Create.html 	modified:   templates/Index.html

--- Host.py
+++ Host.py
@@ -2,10 +2,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+files = ['Index.html', 'Create.html', 'script.js']
 # This tells that the Index file needs to be shown. Flask will look in a folder called temeplates.
 @app.route('/')
 def index():
-    return render_template('Index.html')
+    return render_template(files)
 
 # Runs it in debug mode.
 if __name__ == '__main__':
