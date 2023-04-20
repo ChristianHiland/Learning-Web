5e54363a66df5ff076a84e715a976b56dc6eee05
Christian Hiland <hilandchristian112@gmail.com>
2023-04-20 09:01:50 -0400

    Update Host.py

--- Host.py
+++ Host.py
@@ -1,8 +1,12 @@
+# Importings Flask, and render_template.
 from flask import Flask, render_template
 app = Flask(__name__)
 
+# This tells that the Index file needs to be shown. Flask will look in a folder called temeplates.
 @app.route('/')
 def index():
     return render_template('Index.html')
+
+# Runs it in debug mode.
 if __name__ == '__main__':
     app.run(debug=True)
