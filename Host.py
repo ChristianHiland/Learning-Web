e7fbf850895e53d5d7cde99987fa4155230c2cc5
Christian Hiland <hilandchristian112@gmail.com>
2023-04-21 12:27:31 -0400

    Add files via upload

--- Host.py
+++ Host.py
@@ -5,9 +5,12 @@ app = Flask(__name__)
 files = ['Index.html', 'Create.html', 'script.js']
 # This tells that the Index file needs to be shown. Flask will look in a folder called temeplates.
 @app.route('/')
-def index():
-    return render_template(files)
-
+def Index():
+    return render_template("Index.html")
+@app.route('/Create.html')
+def Create():
+    return render_template("Create.html")
+    
 # Runs it in debug mode.
 if __name__ == '__main__':
     app.run(debug=True)
